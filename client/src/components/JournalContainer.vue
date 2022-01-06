10eb9f4d4dc7e00a3a40536841e5c9c273733314
Katherine Blackstone <kr309281@unknown84d81bfa1178.attlocal.net>
2022-01-06 15:58:36 -0600

    Fetching data from server, added entries endpoint

--- client/src/components/JournalContainer.vue
+++ client/src/components/JournalContainer.vue
@@ -1,47 +1,37 @@
 <template>
-  <div class="journal-container">
-    <template v-for="card in cardData" :key="card.id">
-      <JournalCard :journalData="card"/>
+  <div class="journal-container" v-if="!loading">
+    <template v-for="entry in journalData" :key="entry.date">
+      <JournalCard :journalData="entry" :allCardData="cardData"/>
     </template>
   </div>
 </template>
 
 <script>
+/*eslint-disable vue/no-v-for-template-key*/
 import getMonthName from '../utils/getMonthName';
 import JournalCard from './JournalCard.vue';
+/* TODO - improve data transfer between App -> JournalContainer -> JournalCard, 
+especially with the card data. I dislike sending the entire 78 obj array to each JournalCard to filter 
+within the component, only the needed card obj should be sent.
+Likely will align with lazy loading so App or JournalContainer only has to generate however many objects for 1 screen
+Perhaps JournalContainer could get the 10,15 entries to show & generate 1 object to send to JournalCard?
+*/
 export default {
-  // props: {
-  //   data: Object,
-  // },
   data() {
     return {
-      cardData: [{
-      id: "swkn",  //switching to unique int value in future
-      reversed: false,
-      date: "Jan 5, 2021", //turn this into a Date obj
-      journalEntry: "Chupa chups pie brownie gingerbread candy liquorice powder cupcake soufflé. Apple pie pudding biscuit candy canes macaroon. Caramels gingerbread danish chupa chups cupcake lemon drops jelly beans.",
-    },{
-      id: "cu05",
-      reversed: true,
-      date: "Jan 4, 2021",
-      journalEntry: null
-    },{
-      id: "wa07",
-      reversed: false,
-      date: "Jan 3, 2021",
-      journalEntry: "Lollipop candy canes oat cake dragée sugar plum sweet roll apple pie cookie fruitcake. Biscuit icing chupa chups tart powder caramels. Sugar plum candy canes cookie cake jelly-o. Apple pie biscuit pudding dragée wafer.\nCake sesame snaps bonbon lemon drops fruitcake. Dessert muffin biscuit candy canes gingerbread. Cheesecake liquorice topping sweet roll cupcake shortbread toffee sugar plum.",
-    },{
-      id: "ma08",
-      reversed: true,
-      date: "Jan 2, 2021",
-      journalEntry: null,
-    },{
-      id: "ma22",
-      reversed: false,
-      date: "Jan 1, 2021",
-      journalEntry: null,
-    },]
+      cardData: [],
+      journalData: [],
+      loading: true,
     }
+  },
+  async mounted() {
+    this.journalData = await fetch("http://localhost:8080/entries")
+      .then(res => res.json())
+    console.log(this.journalData)
+    this.cardData = await fetch("http://localhost:8080/cards")
+      .then(res => res.json())
+    console.log(this.cardData)
+    this.loading = false;
   },
   computed: {
     getFormattedDate() {
