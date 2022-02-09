480f8309bfc156eb97ad3c2a0d81ad83bc117fbc
Katie Blackstone <katierblackstone@gmail.com>
2022-02-08 21:18:01 -0600

    re-implementing firebase

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+//import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [vue()],
+// });
+const config = {
   plugins: [vue()],
-});
+};
+
+export default config;
---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+/*
+TODO:
+- set up functions - copying over express server logic
+  https://medium.com/@savinihemachandra/creating-rest-api-using-express-on-cloud-functions-for-firebase-53f33f22979c
+- confirm functions are working
+- set up firestore
+- put in dummy data
+*/
